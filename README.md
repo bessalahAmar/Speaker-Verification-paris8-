10b64583a97478146af568d4256a0a2af4b93690
MB <gumsak@users.noreply.github.com>
2020-06-16 00:40:42 +0200

    Added the script to train a model

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ steps to run : <br>
 2) ./extract-features.sh
 3) ./normalization-features.sh
 
+# We need .ndx files to train the models
+
+Their content looks like this:
+
+- mmm f1 f2 f3
+
+- mmm: name of the speaker model to create
+- f1, f2, etc: name of the feature files used to create it
+
 # Crédit 
 https://alize.univ-avignon.fr/ <br>
 https://www.irisa.fr/metiss/guig/spro/spro-4.0.1/spro_3.html
--- extract-features.sh
+++ extract-features.sh
@@ -1,11 +1,14 @@
 ls data/wav/clients/*.wav | sed 's-data/wav/clients/-- ; s-\.wav--' > data/data.lst
-mkdir -p data/prm/clients
+#mkdir -p data/prm/clients
 
 for i in `cat data/data.lst`;do
-   COMMAND_LINE="sfbcep -f 16000 -p 14 -r 24 -e -Z -R -L 400 -D -A data/wav/clients/$i.wav data/prm/$i.tmp.prm"
+   COMMAND_LINE="sfbcep -m -k 0.97 -p19 -n 24 -r 22 -e -D -A data/wav/clients/$i.wav data/prm/$i.tmp.prm"
    echo $COMMAND_LINE
    $COMMAND_LINE
 done 
+
+#original command: sfbcep -f 16000 -p 14 -r 24 -e -Z -R -L 400 -D -A
+#new command: sfbcep -p 19 -r 22 -e -Z -R -L 400 -D -A -F wave
 
 # -m -k 0.97 -p19 -n 24 -r 22 -e -D -A
 # -m -k 0.97 -p19 -n 20 -r 22 -e -D -A
