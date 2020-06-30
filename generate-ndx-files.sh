b6e13ac5a462ff9284707b8661f1e7d8bbf9b049
MB <gumsak@users.noreply.github.com>
2020-06-30 23:45:18 +0200

    Update: now splitting data for Training/Test, Using TrainTarget

--- generate-ndx-files.sh
+++ generate-ndx-files.sh
@@ -3,16 +3,40 @@
 # 1 Make the 'ndx' directory
 	mkdir -p ndx
 
-# 2 Generate the trainModel ndx file
+# 2 Split the data files in 2: some data used for the training, and the rest used for the tests
+	echo -n "" > data/trainList.lst
+	echo -n "" > data/testList.lst
 	i=1
-	echo -n "" > ndx/trainModel.ndx
 	for element in $(cat data/data.lst);do
-		j=$(printf "%02d" $i)
-		echo "spk$j $element" >> ndx/trainModel.ndx
+		# if the file from the list is even, use it for training
+		if (( $i % 2 ));then
+			echo "$element" >> data/trainList.lst
+		# else, keep it for the tests
+		else
+			echo "$element" >> data/testList.lst
+		fi
 		i=$((i+1))	
 	done
 
-# 3 Generate the computetest GMM ndx file
+# 3 Generate the trainModel ndx file (select the files where the speaker says numbers)
+	i=1
+	tmp=""
+	k=0
+	echo -n "" > ndx/trainModel.ndx
+	for element in $(cat data/trainList.lst);do
+		tmp+=" $element"
+		if [[ $k -ge 4 ]];then
+			#j=$(printf "%02d" $i)
+			j=$(echo $element | cut -c1-3)
+			echo "spk$j$tmp" >> ndx/trainModel.ndx
+			i=$((i+1))
+			tmp=""
+			k=-1
+		fi
+		k=$((k+1))	
+	done
+
+# 4 Generate the computetest GMM ndx file. It contains the name of the test files in the first column, followed by the names of the models that will be compared to them
 	i=0
 	j=0
 	#To limit the amount of speaker to get from the list, uncomment the line below and the 'if' in lines 37 & 51, and the 'i' & 'j' incrementation
@@ -44,7 +68,7 @@
 		# <end> ####
 	done < "$file"
 
-	for element in $(cat data/data.lst);do
+	for element in $(cat data/testList.lst);do
 		
 		echo "$element$word" >> ndx/computetest_gmm_target-seg.ndx
 		
--- train-model.sh
+++ train-model.sh
@@ -48,9 +48,9 @@
 	echo "		done, see log/TrainWorld.log for details"
 
 # 2. Speaker GMM model adaptation
-#	echo "Train Speaker dependent GMMs"
-#	bin/TrainTarget --config cfg/TrainTarget.cfg &> log/TrainTarget.cfg
-#	echo "		done, see log/TrainTarget.cfg for details"
+	echo "Train Speaker dependent GMMs"
+	bin/TrainTarget --config cfg/TrainTarget.cfg &> log/TrainTarget.cfg
+	echo "		done, see log/TrainTarget.cfg for details"
 
 # 3. Speaker model comparison
 	echo "Compute Likelihood"
